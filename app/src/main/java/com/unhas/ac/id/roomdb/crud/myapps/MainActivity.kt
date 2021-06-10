227a53c2c53d3fb98d0adc443d9e1cd7381f3f22
iffatainiyyah <iffatainiyyah@gmail.com>
2021-06-10 10:30:32 +0800

    Updated Main Activity and changes color for UI

--- app/src/main/java/com/unhas/ac/id/roomdb/crud/myapps/MainActivity.kt
+++ app/src/main/java/com/unhas/ac/id/roomdb/crud/myapps/MainActivity.kt
@@ -2,12 +2,37 @@ package com.unhas.ac.id.roomdb.crud.myapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.unhas.ac.id.roomdb.crud.myapps.databinding.ActivityMainBinding
+import com.unhas.ac.id.roomdb.crud.myapps.db.TaskAdapter
+import com.unhas.ac.id.roomdb.crud.myapps.viewmodel.TaskViewModel
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: TaskAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var taskModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        taskModel = ViewModelProviders.of(this).get(TaskViewModel::class.java)
 
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = TaskAdapter(taskModel)
+        recyclerView = binding.TaskRV
+        recyclerView.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+
+        }
     }
 }
